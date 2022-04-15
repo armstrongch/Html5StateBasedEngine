cde0be47104c4538c4ed6b82cad3ccadb6550efb
armstrongch <christopherernestarmstrong@gmail.com>
2022-04-15 10:17:30 -0400

    button styling and title state'

--- game.js
+++ game.js
@@ -1,4 +1,23 @@
 var game =
 {
-
+	title_state:
+		['title'],
+	test_state_1:
+		['test_row1', 'test_row3'],
+	test_state_2:
+		['test_row1', 'test_row2', 'test_row4'],
+	test_state_3:
+		['test_row1', 'test_row5'],
+	
+	load_state: function(state)
+	{
+		
+		$('tr').css('visibility', 'hidden');
+		$('tr').css('display', 'none');
+		for (let i = 0; i < this[state].length; i += 1)
+		{
+			$(document.getElementById(this[state][i])).css('visibility', 'visible');
+			$(document.getElementById(this[state][i])).css('display', '');
+		}			
+	}
 };
