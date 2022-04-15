6dfe43e3ae36731186eef35ffdd2d6159a542494
armstrongch <christopherernestarmstrong@gmail.com>
2022-04-15 12:46:44 -0400

    itch compatibility changes

--- game.js
+++ game.js
@@ -3,41 +3,43 @@ var game =
 	title_state:
 		['title'],
 	test_state_1:
-		['list_of_stuff','test_row1', 'test_row3'],
+		['list_of_stuff', 'list_of_stuff_two_cols', 'test_row1', 'test_row3'],
 	
 	load_game: function()
 	{
 		this.load_state('title_state');
+		
 		list_item.AddListItemToRow(
 			'list_of_stuff', 0,
-			'Placeholder.png',
-			'Test Text'
-		);
-		list_item.AddListItemToRow(
-			'list_of_stuff', 0,
-			'Placeholder.png',
+			'placeholder.png',
 			'Different Test Text.'
 		);
 		list_item.AddListItemToRow(
 			'list_of_stuff', 0,
-			'Placeholder.png',
+			'placeholder.png',
 			'Text With Line Breaks<br/>More Stuff<br/>Additional Info'
 		);
 		
 		list_item.AddListItemToRow(
-			'list_of_stuff', 1,
-			'Placeholder.png',
+			'list_of_stuff_two_cols', 0,
+			'placeholder.png',
 			'Test Text'
 		);
 		list_item.AddListItemToRow(
-			'list_of_stuff', 1,
-			'Placeholder.png',
+			'list_of_stuff_two_cols', 0,
+			'placeholder.png',
 			'Different Test Text.'
 		);
 		list_item.AddListItemToRow(
-			'list_of_stuff', 1,
-			'Placeholder.png',
+			'list_of_stuff_two_cols', 1,
+			'placeholder.png',
 			'Text With Line Breaks<br/>More Stuff<br/>Additional Info'
+		);
+		
+		list_item.AddListItemToRow(
+			'list_of_stuff_two_cols', 1,
+			'placeholder.png',
+			'Test Text'
 		);
 	},
 	
--- list_item.js
+++ list_item.js
@@ -2,9 +2,10 @@ var list_item =
 {
 	GetListItemHTML: function(image_file_name, item_text)
 	{
-		return `<div style='clear:both'><p class='list_item_text'><img class='list_item_image' src='Images/${image_file_name}'/>${item_text}</p></span></div>`;
+		return `<div style='clear:both'><p class='list_item_text'><img class='list_item_image' src='./Images/${image_file_name}'/>${item_text}</p></span></div>`;
 	},
 	
+	//for itch compatibility, file names are CASE SENSITIVE!
 	AddListItemToRow: function(row_name, column, image_file_name, item_text)
 	{
 		$(document.getElementById(row_name)).children().eq(column).append(
