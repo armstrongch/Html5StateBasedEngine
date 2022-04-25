c477cfbb4faa6409384e482ff103fb5281034407
armstrongch <christopherernestarmstrong@gmail.com>
2022-04-25 07:12:33 -0400

    added support for local saving

--- game.js
+++ game.js
@@ -1,20 +1,5 @@
 var game =
 {
-	save_mode: 'server',
-	save_list: [],
-	
-	check_save_mode: function()
-	{
-		test_save_list = [{key: 'test', value: 'test'}];
-		this.save(test_save_list);
-		var test_load_list = this.load();
-		if (test_load_list.length == 0)
-		{
-			this.save_mode = 'local';
-			$('#save_button').html("<button onclick='game.save_local()>Save Game</button>");
-		}
-	},
-	
 	load_state: function(state)
 	{
 		$('table').css('display', '');
@@ -25,87 +10,5 @@ var game =
 			$(document.getElementById(this[state][i])).css('visibility', 'visible');
 			$(document.getElementById(this[state][i])).css('display', '');
 		}			
-	},
-	
-	//saves a list of key-value pairs
-	save: function(save_list)
-	{
-		if (this.save_mode == 'server')
-		{
-			this.auto_save(save_list);
-		}
-		else
-		{
-			this.save_list = save_list;
-		}
-	},
-	
-	load: function()
-	{
-		if (this.save_mode == 'server')
-		{
-			return this.load_server();
-		}
-		else
-		{
-			return this.load_local();
-		}
-	},
-	
-	save_local: function()
-	{
-		var save_string = "";
-		for (let i = 0; i < this.save_list.length; i += 1)
-		{
-			save_string += this.save_list[i].key + "=" + this.save_list[i].value + ",";
-		}
-		var bb = new Blob([fileContent ], { type: 'text/plain' });
-		var a = document.createElement('a');
-		a.download = 'save_game.sav';
-		a.href = window.URL.createObjectURL(bb);
-		a.click();
-	},
-	
-	auto_save: function(save_list)
-	{
-		var path = "path=\;";
-		const d = new Date();
-		d.setTime(d.getTime() + (365*24*60*1000));
-		var expires = "expires=" + d.toUTCString() + ";";
-		var same_site = "SameSite=None;secure;";
-		
-		for (let i = 0; i < save_list.length; i += 1)
-		{
-			var new_cookie = save_list[i].key + "=" +  save_list[i].value + ";";
-			new_cookie += path;
-			new_cookie += same_site;
-			new_cookie += expires;
-			document.cookie = new_cookie;
-		}
-	},
-	
-	load_server: function()
-	{
-		var load_list = [];
-		var split_cookies = document.cookie.split(';');
-		for (let i = 0; i < split_cookies.length; i += 1)
-		{
-			var key_value_array = split_cookies[i].split('=');
-			if (key_value_array.length > 1)
-			{
-				load_list.push(
-					{ 
-						key: key_value_array[0].slice().trim(),
-						value: key_value_array[1].slice().trim()
-					}
-				);
-			}
-		}
-		return load_list;
-	},
-	
-	load_local: function()
-	{
-		return [];
 	}
 };
--- pet_picker_content.js
+++ pet_picker_content.js
@@ -1,9 +1,16 @@
 var pet_picker_content =
 {
-	load_game: function()
+	setup_game: function()
 	{
 		this.init_states();
 		this.setup_page();
+	},
+	
+	begin_game: function()
+	{
+		this.load_pet_picks();
+		list_item.AddListItemsToRow('list_of_pets', this.list_of_pets);
+		game.load_state('pet_selection_state');
 	},
 	
 	init_states: function()
@@ -15,11 +22,12 @@ var pet_picker_content =
 	
 	setup_page: function()
 	{
-		game.check_save_mode();
+		save.initialize_save('pet_picker');
 		this.load_audio();
 		game.load_state('title_state');
 		this.setup_pet_list();
 		this.load_pet_picks();
+		
 		list_item.AddListItemsToRow('list_of_pets', this.list_of_pets);
 		list_item.AppendHTMLItemToRow(
 			'pet_picker', 0, lists.generate_dropdown_from_list(this.list_of_pets,'pet_picker_select'));
@@ -59,12 +67,12 @@ var pet_picker_content =
 		{
 			save_list.push({ key: this.list_of_pets[i].item_name, value: this.list_of_pets[i].pet_picks });
 		}
-		game.save(save_list);
+		save.save(save_list);
 	},
 	
 	load_pet_picks: function()
 	{
-		var load_list = game.load();
+		var load_list = save.load();
 		for (let i = 0; i < load_list.length; i += 1)
 		{
 			var pet_name = load_list[i].key;
@@ -100,6 +108,7 @@ var pet_picker_content =
 		list_item.AddListItemsToRow('list_of_pets', this.list_of_pets);
 		sound_manager.play_sound_by_name('click');
 		this.save_pet_picks();
+		
 		game.load_state('post_pick_state');
 	},
 	
